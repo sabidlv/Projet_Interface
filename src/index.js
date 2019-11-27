d060865e88ebc897e585fba20fa49473321bfac3
sabidlv <49045493+sabidlv@users.noreply.github.com>
2019-11-27 16:38:38 +0100

    fix merge error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
+
 import config from './config';
 import * as firebase from 'firebase/app';
 import 'firebase/database';
 firebase.initializeApp(config);
-=======
 
-  import * as firebase from 'firebase/app';
-  import 'firebase/database';  
 
   var config = {
     apiKey: "AIzaSyCImrYHsVo251bni1yRSMUjqWeoS9MXcms",
@@ -14,9 +11,7 @@ firebase.initializeApp(config);
     databaseURL: "https://interface-581ae.firebaseio.com",
     storageBucket: "interface-581ae.appspot.com"
   };
- 
-  firebase.initializeApp(config);
 
   // Get a reference to the database service
   var database = firebase.database().ref();
->>>>>>> bc5decc514b83ae59fd6ff99e0350cc82b43e17c
+
