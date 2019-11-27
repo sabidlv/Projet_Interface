bc5decc514b83ae59fd6ff99e0350cc82b43e17c
sabrina delvaux <sabidlv@homail.com>
2019-11-27 13:14:10 +0100

    connection to database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import config from './config';
 
-import * as firebase from 'firebase/app';
-import 'firebase/database';
+  import * as firebase from 'firebase/app';
+  import 'firebase/database';  
 
+  var config = {
+    apiKey: "AIzaSyCImrYHsVo251bni1yRSMUjqWeoS9MXcms",
+    authDomain: "interface-581ae.firebaseapp.com",
+    databaseURL: "https://interface-581ae.firebaseio.com",
+    storageBucket: "interface-581ae.appspot.com"
+  };
+ 
+  firebase.initializeApp(config);
 
-firebase.initializeApp(config);
+  // Get a reference to the database service
+  var database = firebase.database().ref();
