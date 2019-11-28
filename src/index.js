ddf8e9d4aad2b4ba25b74c3570f809e4e6a07615
sabrina delvaux <sabidlv@homail.com>
2019-11-28 10:44:36 +0100

    set and get

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
   import * as firebase from 'firebase/app';
   import 'firebase/database';  
 
@@ -9,7 +8,57 @@
     storageBucket: "interface-581ae.appspot.com"
   };
  
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
+var database = firebase.database();//.ref('WAD/');
+let data = "";
 
-  // Get a reference to the database service
-  var database = firebase.database().ref();
+document.getElementById('game').addEventListener('click',()=>{
+  console.log('les GAME');
+ document.getElementById('container-msg').innerHTML="";
+  data = database.ref('GAME/');
+  data.on('child_added', function(snap){
+    //console.log('les valeurs'+snap.val());
+  let enfant= document.createElement('p');
+  console.log(snap.val());
+  let text = document.createTextNode(snap.val().msg);
+  enfant.appendChild(text);
+  document.getElementById('container-msg').appendChild(enfant);
+  });
+});
+
+document.getElementById('web').addEventListener('click',()=>{
+  document.getElementById('container-msg').innerHTML="";
+   data = database.ref('WEB/');
+  data.on('child_added', function(snap){
+    //console.log('les valeurs'+snap.val());
+  let enfant= document.createElement('p');
+  console.log(snap.val());
+  let text = document.createTextNode(snap.val().msg);
+  enfant.appendChild(text);
+  document.getElementById('container-msg').appendChild(enfant);
+  });
+});
+
+//pusher un post
+let mybtn = document.getElementById('btn-envoie');
+mybtn.addEventListener('click', function(event){
+    event.preventDefault();
+    let msg = document.getElementById('msg').value;
+    console.log('message '+ msg);
+    data.push({msg});
+    document.getElementById('msg').value='';
+    document.getElementById('msg').focus();
+ 
+});
+//user:
+let database1 = firebase.database().ref('USER/');
+let btnUser = document.getElementById('btnUser');
+btnUser.addEventListener('click', function(event){
+    event.preventDefault();
+    let user = document.getElementById('user').value;
+    console.log('message '+ user);
+    database1.push({msg});
+
+ 
+});
+
