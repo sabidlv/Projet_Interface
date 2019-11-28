e46b9811e790d773479e3e244109e520b9600414
Aurelie Debay <a.debay@interface3.be>
2019-11-28 10:57:34 +0100

    change style

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
 
-import config from './config';
-import * as firebase from 'firebase/app';
-import 'firebase/database';
-firebase.initializeApp(config);
-
-
-  var config = {
-    apiKey: "AIzaSyCImrYHsVo251bni1yRSMUjqWeoS9MXcms",
-    authDomain: "interface-581ae.firebaseapp.com",
-    databaseURL: "https://interface-581ae.firebaseio.com",
-    storageBucket: "interface-581ae.appspot.com"
-  };
-
-  // Get a reference to the database service
-  var database = firebase.database().ref();
-
