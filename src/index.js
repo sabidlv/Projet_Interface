e1c95eb4eb3460c16e328690c511e0b16b9e1e7b
sabrina delvaux <sabidlv@homail.com>
2019-11-29 12:43:49 +0100

    resolve bug

--- src/index.js
+++ src/index.js
@@ -1,57 +1,59 @@
   import * as firebase from 'firebase/app';
   import 'firebase/database';  
+  import { ajoutMessageTchat} from './app/helpers';
+
   var config = {
     apiKey: "AIzaSyCImrYHsVo251bni1yRSMUjqWeoS9MXcms",
     authDomain: "interface-581ae.firebaseapp.com",
     databaseURL: "https://interface-581ae.firebaseio.com",
     storageBucket: "interface-581ae.appspot.com"
   };
+  class ObjMessage{
+    constructor(user, msg){
+      this.user= user;
+      this.msg= msg;
+    }
+  }
+    class ObjUser{
+    constructor(user, groupe){
+      this.user = user;
+      this.groupe= groupe;
+    }
+}
+let data = "";
+let user= " ";
+
 firebase.initializeApp(config);
 const database = firebase.database();
 const database1 = firebase.database().ref('USER/');
 const database2 = firebase.database().ref();
-const query = database2.child('USER').orderByChild("user").equalTo('cc');
+const query = database2.child('USER').orderByChild("user");//.equalTo('value');
 
-
-let data = "";
-let user= " ";
-
-query.on('child_added',function(snap){
-  let data =snap.val();
-    console.log(data);
-  });
-
-
-
-class ObjMessage{
-  constructor(user, msg){
-    this.user= user;
-    this.msg= msg;
-  }
-}
-class ObjUser{
-  constructor(user, groupe){
-    this.user = user;
-    this.groupe= groupe;
-  }
-}
 // se loguer avec son alias
 document.getElementById('btn-login-alias').addEventListener('click', ()=>{
   user = document.getElementById('alias').value;
-
+  query.once('value',function(snapshot){
+    console.log(snapshot.val())
+    snapshot.forEach(snap => {
+      if(snap.val().user === user){
+        console.log(snap.val().groupe);
+        document.getElementById(snap.val().groupe).setAttribute('style', 'visibility: visible;');
+        document.getElementById('label-alias').setAttribute('style','visibility: hidden;');
+        document.getElementById('alias').setAttribute('style','visibility: hidden;');
+      }
+    });
+  });
 });
 
-  
 // se faire un alias, creation
 document.getElementById('btn-save-alias').addEventListener('click',()=>{
   user = document.getElementById('alias').value;
-  let groupe = "WAD";
+  let groupe = document.getElementById('alias-groupe').value;
   const objUtilisateur = new ObjUser(user, groupe);
   database1.push(objUtilisateur);
   document.getElementById('alias').value='';
+  document.getElementById('alias-groupe').value='';
 });
-
-
 
 // tchat
 document.getElementById('game').addEventListener('click',()=>{
@@ -71,25 +73,6 @@ document.getElementById('wad').addEventListener('click',()=>{
   ajoutMessageTchat(data, user);
 });
 
-// function pour afficher le tchat
-function ajoutMessageTchat(data, user){
-  document.getElementById('container-msg').innerHTML="";
-  data.on('child_added', function(snap){
-  let mydiv = document.createElement('div');
-  let enfant1 = document.createElement('p');  
-  let enfant= document.createElement('p');
-   //enfant.classList.add('red');
-   //enfant.classList.add('darken-3');
-   let alias = document.createTextNode(snap.val().objmsg.user);
-   console.log(snap.val());
-   let text = document.createTextNode(snap.val().objmsg.msg);
-   enfant1.appendChild(alias);
-   enfant.appendChild(text);
-   document.getElementById('container-msg').appendChild(mydiv);
-   mydiv.appendChild(enfant1);  
-   mydiv.appendChild(enfant);
-  });
-}; 
 //pusher un post
 let mybtn = document.getElementById('btn-envoie');
 mybtn.addEventListener('click', function(event){
