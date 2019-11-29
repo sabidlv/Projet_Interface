b76df1f0bcc22a07cd4932cf0fc31048e2e85beb
sabrina delvaux <sabidlv@homail.com>
2019-11-29 09:26:09 +0100

    try with query

--- src/index.js
+++ src/index.js
@@ -7,48 +7,87 @@
     storageBucket: "interface-581ae.appspot.com"
   };
 firebase.initializeApp(config);
-var database = firebase.database();
+const database = firebase.database();
+const database1 = firebase.database().ref('USER/');
+const database2 = firebase.database().ref();
+const query = database2.child('USER').orderByChild("user").equalTo('cc');
+
+
 let data = "";
-let user='';
+let user= " ";
+
+query.on('child_added',function(snap){
+  let data =snap.val();
+    console.log(data);
+  });
+
+
+
 class ObjMessage{
   constructor(user, msg){
     this.user= user;
     this.msg= msg;
   }
 }
-// se faire un alias
-document.getElementById('btn-alias').addEventListener('click',()=>{
+class ObjUser{
+  constructor(user, groupe){
+    this.user = user;
+    this.groupe= groupe;
+  }
+}
+// se loguer avec son alias
+document.getElementById('btn-login-alias').addEventListener('click', ()=>{
   user = document.getElementById('alias').value;
-  console.log(user);
-  document.getElementById('alias').value='';
 
 });
 
+  
+// se faire un alias, creation
+document.getElementById('btn-save-alias').addEventListener('click',()=>{
+  user = document.getElementById('alias').value;
+  let groupe = "WAD";
+  const objUtilisateur = new ObjUser(user, groupe);
+  database1.push(objUtilisateur);
+  document.getElementById('alias').value='';
+});
+
+
+
+// tchat
 document.getElementById('game').addEventListener('click',()=>{
   data = database.ref('GAME/');
   ajoutMessageTchat(data, user);
 });
-
 document.getElementById('web').addEventListener('click',()=>{
   data = database.ref('WEB/');
   ajoutMessageTchat(data, user);
-
+});
+document.getElementById('general').addEventListener('click',()=>{
+  data = database.ref('GENERAL/');
+  ajoutMessageTchat(data, user);
+});
+document.getElementById('wad').addEventListener('click',()=>{
+  data = database.ref('WAD/');
+  ajoutMessageTchat(data, user);
 });
 
-// function
+// function pour afficher le tchat
 function ajoutMessageTchat(data, user){
   document.getElementById('container-msg').innerHTML="";
   data.on('child_added', function(snap){
-   let enfant1 = document.createElement('p');  
-   let enfant= document.createElement('p');
-   enfant.classList.add('red');
-   enfant.classList.add('darken-3');
+  let mydiv = document.createElement('div');
+  let enfant1 = document.createElement('p');  
+  let enfant= document.createElement('p');
+   //enfant.classList.add('red');
+   //enfant.classList.add('darken-3');
    let alias = document.createTextNode(snap.val().objmsg.user);
+   console.log(snap.val());
    let text = document.createTextNode(snap.val().objmsg.msg);
    enfant1.appendChild(alias);
    enfant.appendChild(text);
-   document.getElementById('container-msg').appendChild(enfant1);  
-   document.getElementById('container-msg').appendChild(enfant);
+   document.getElementById('container-msg').appendChild(mydiv);
+   mydiv.appendChild(enfant1);  
+   mydiv.appendChild(enfant);
   });
 }; 
 //pusher un post
@@ -57,7 +96,6 @@ mybtn.addEventListener('click', function(event){
     event.preventDefault();
     let msg = document.getElementById('msg').value;
     const objmsg = new ObjMessage(user,msg);
-    console.log('message '+ objmsg);
     data.push({objmsg});
     document.getElementById('msg').value='';
     document.getElementById('msg').focus();
